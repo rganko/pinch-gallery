dffad2ff36e48dfceacd99723f827aac772af602
Robert Ganko <robert.ganko@gmail.com>
2023-03-04 20:17:05 +0100

    init cra

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1 className='color-orange'>Masz pytania?</h1>
+      <h1 className='color-orange'>Nasz specjalista chętnie Ci pomoże.</h1>
+
+      <h4>Eleonora Frąckiewiczówna</h4>
+      <h4>Specjalisty ds. finansowania</h4>
+
+      <p>efranckiewiczowna@rtc.pl</p>
+      <p>500 677 543</p>
+    </div>
+  );
+}
+
+export default App;
